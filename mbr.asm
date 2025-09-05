7b8312db426f81e4a1522ca00685db8d6adc244b
mamutrahal269 <levrymvc@gmail.com>
2025-09-05 11:29:14 +0700

    mbr.asm, stage2.asm

---
+++ mbr.asm
@@ -0,0 +1,134 @@
+%define LODSECTRS 1
+bits 16
+org 0x7C00
+main16:
+    mov [boot_drive], dl
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7BFF
+    sti
+
+    mov ah,0x41
+    mov bx, 0x55AA
+    mov dl, [boot_drive]
+    int 13h
+    jc unsup_lba
+    cmp bx, 0xAA55
+    jne unsup_lba
+    mov si, dap
+    mov ah, 0x42
+    mov dl, [boot_drive]
+    int 13h
+    jc disk_error
+
+    mov ah, 0x06
+    mov al, 0
+    mov bh, 0x07
+    mov cx, 0
+    mov dx, 184Fh
+    int 0x10
+
+.a20_check:
+    push es
+    push ds
+    mov ax, 0x0000
+    mov es, ax
+    mov ax, word[es:0x7DFE]
+    mov dx, 0xFFFF
+    mov ds, dx
+    mov dx, word[ds:0x7E0E]
+    cmp ax, dx
+    jne .after
+    shl ax, 8
+    mov word[es:0x7DFE], ax
+    mov dx, word[ds:0x7E0E]
+    cmp dx, ax
+    pop ds
+    pop es
+    jne .after
+
+.enable_a20:
+    in al, 0x92
+    test al, 2
+    jnz .after
+    or al, 2
+    and al, 0xFE
+    out 0x92, al
+.after:
+
+    jmp 0x7E00
+;----------------------------------------------------
+bits 16
+disk_error:
+    mov si, disk_msg
+    push ax
+    call print_msg
+    pop ax
+    mov al, ah
+    call bout
+    mov si, newline
+    call print_msg
+    jmp stop
+unsup_lba:
+    mov si, lba_err
+    call print_msg
+    jmp stop
+bout:
+    push ax
+    shr al, 4
+    call nout
+    pop ax
+nout:
+    and al, 0x0F
+    daa
+    add al, 0xF0
+    adc al, 0x40
+cout:
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+    ret
+
+print_msg:
+    mov ah, 0x0E
+    lodsb
+    test al, al
+    jz .done
+    mov bl, 0x00
+    int 10h
+    jmp print_msg
+.done:
+    ret
+
+stop:
+    mov cx, 15
+    mov ah, 0x86
+    int 0x15
+    int 0x18
+
+;----------------------------------------------------
+dap:
+                    db 0x10
+                    db 0
+    .sectors        dw LODSECTRS
+    .buffer_offset  dw 0x7E00
+    .buffer_segment dw 0
+    .lba_low        dd 0x1
+    .lba_high       dd 0
+;----------------------------------------------------
+
+disk_msg db 'Disk read error! Code: 0x', 0
+lba_err db 'Your storage device does not support LBA.', 0x0D, 0x0A, 0
+newline db 0x0D, 0x0A, 0
+
+%if LODSECTRS > 127
+    %error "too many sectors"
+%endif
+
+times 509 - ($-$$) db 0
+boot_drive db 0
+dw 0xAA55
+
---
+++ stage2.asm
@@ -0,0 +1,198 @@
+org 0x7E00
+bits 16
+%define LODSECTRS 1
+%define DATA_LOAD_ADDRESS code_end + 1
+%define DESTINATION_ADDRESS 0x100000
+%define BOOT_DRIVE byte [0x7C00 + 509]
+
+%macro SWITCH_PM 0
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp 0x08:%%PM
+bits 32
+%%PM:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x7BFF
+
+%endmacro
+
+%macro SWITCH_RM 0
+    cli
+    mov eax, cr0
+    and eax, 0xFFFFFFFE
+    mov cr0, eax
+
+    jmp 0:%%RM
+bits 16
+%%RM:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov sp, 0x7BFF
+
+    sti
+%endmacro
+
+code_begin:
+    cld
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7BFF
+    sti
+
+%if 0
+    mov al, 0x10             ; не знаю почему,но без этого dap ломается. можно поробовать убрать
+    mov [dap.size], al
+    mov eax, 0
+    mov [dap.res], al
+    mov [dap.sectors], ax
+    mov [dap.buffer_segment], ax
+    mov dword[dap.lba_high], eax
+    mov ax, DATA_LOAD_ADDRESS
+    mov [dap.buffer_offset], ax
+    mov eax, 2
+    mov [dap.lba_low], eax
+%endif
+
+
+%if LODSECTRS <= 127
+    mov ecx, LODSECTRS
+    mov [dap.sectors], cx
+    mov ah, 42h
+    mov si, dap
+    mov dl, BOOT_DRIVE
+    int 13h
+    jc disk_error
+
+    SWITCH_PM
+
+    mov esi, DATA_LOAD_ADDRESS
+    mov edi, DESTINATION_ADDRESS
+    mov ecx, LODSECTRS * 512
+    rep movsb
+
+    jmp DESTINATION_ADDRESS
+%else
+%error "невозможно загрузить >127 секторов"
+    mov cx, 127
+    mov [dap.sectors], cx
+    mov ecx, LODSECTRS
+    mov edi, DESTINATION_ADDRESS
+loadloop:
+    mov ah, 42h
+    mov si, dap
+    mov dl, BOOT_DRIVE
+    int 13h
+    jc disk_error
+
+    add dword[dap.lba_low], 127
+    adc dword[dap.lba_high], 0
+
+    SWITCH_PM
+
+    mov esi, DATA_LOAD_ADDRESS
+    push ecx
+    mov ecx, 127 * 512
+    rep movsb
+    pop ecx
+
+    SWITCH_RM
+    sub ecx, 127
+    jz done
+    cmp ecx, 127
+    jb last
+    jmp 0:loadloop
+
+last:
+    mov [dap.sectors], cx
+    mov ah, 42h
+    mov dl, BOOT_DRIVE
+    int 13h
+    jc disk_error
+
+    SWITCH_PM
+
+    mov esi, DATA_LOAD_ADDRESS
+    mov edx, 512
+    mov eax, ecx
+    mul edx
+    mov ecx, eax
+    rep movsb
+
+    jmp DESTINATION_ADDRESS
+
+done:
+    SWITCH_PM
+
+    jmp DESTINATION_ADDRESS
+%endif
+bits 16
+disk_error:
+    mov esi, disk_err_msg
+    xor ebx, ebx
+    mov ah, 0x40
+    call print
+    jmp $
+print:
+    mov edi, 0xB8000
+    add edi, ebx
+.putch:
+    lodsb
+    test al, al
+    jz .ok
+    mov [edi], ax
+    inc edi
+    inc edi
+    jmp .putch
+.ok:
+    ret
+
+gdt_beg:
+    dq 0
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_beg - 1
+    dd gdt_beg
+bits 16
+dap:
+    .size           db 0x10
+    .res            db 0
+    .sectors        dw 0
+    .buffer_offset  dw DATA_LOAD_ADDRESS
+    .buffer_segment dw 0
+    .lba_low        dd 2
+    .lba_high       dd 0
+
+disk_err_msg db '[bootloader : stage 2] Disk read error. Please, reboot the computer', 0
+align 512, db 0
+code_end:
