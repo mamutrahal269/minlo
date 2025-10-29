19152a334f15580b495e46c8d6a8e621f5577fda
mamutrahal269 <levrymvc@gmail.com>
2025-10-29 13:09:16 +0700

    WIP

--- src/disk.asm
+++ src/disk.asm
@@ -1,5 +1,6 @@
+bits 16
 global read_disk
-global disk_geometry
+extern lba2chs
 
 ; input:
 ;	eax   - lba
@@ -10,57 +11,51 @@ global disk_geometry
 ; output:
 ;	flags.CF = 0 - ok
 ;	flags.CF = 1 - error
+section .text
 read_disk:
-	push eax
-	add eax, ecx
-	cmp eax, [si + 6]
-	pop eax
-	ja .failure
 .lp:
-; Вход: 
-;   EAX = LBA адрес (32-битный)
-;   BL = количество головок (heads)
-;   BH = секторов на дорожку (sectors_per_track)
+	push eax
+	push edx
+	push ecx
+	movzx edx, si
+	mov ecx, cyl
+	call lba2chs
+	test eax, eax
+	jnz .failure
+	
+	mov ax, 0x0201
+	mov cx, [cyl]
+	xchg cl, ch
+	shl cl, 6
+	or cl, [sec]
+	mov dh, [head]
+	int 0x13
+	jc .failure
+	
+	pop ecx
+	pop edx
+	pop eax
+	add bx, 512
+	jnc .0
+	push ax
+	mov ax, es
+	add ax, 0x1000
+	mov es, ax
+	pop ax
+.0:
+	inc eax
+	dec ecx
+	jnz .lp
 
-lba_to_chs:
-    push edx
-    push ecx
-    push ebx
-
-    movzx edx, word [si + 2]
-    movzx ebx, word [si + 4]
-    imul edx, ebx
-
-    
-    
-    
-    mov edi, eax           ; EDI = cylinder (сохраняем)
-    
-    ; Вычисляем head и sector
-    mov eax, edx           ; EAX = temp
-    mov edx, 0
-    div ecx                ; EAX = head, EDX = остаток
-    
-    mov esi, eax           ; ESI = head
-    mov ebp, edx           ; EBP = sector - 1
-    inc ebp                ; EBP = sector (1-based)
-    
-    ; Упаковываем для INT 13h:
-    ; CH = cylinder (младшие 8 бит)
-    ; CL[7:6] = cylinder (старшие 2 бита)
-    ; CL[5:0] = sector
-    ; DH = head
-    
-    mov ch, dil            ; CH = cylinder[7:0]
-    mov dh, sil            ; DH = head
-    
-    mov cl, bpl            ; CL = sector
-    mov al, dil            ; AL = cylinder
-    shr al, 2              ; AL[7:6] = cylinder[9:8]
-    and al, 0xC0           ; Оставляем только биты 6-7
-    or cl, al              ; Объединяем с sector
-    
-    pop ebx
-    pop ecx
-    pop edx
-    ret	
+	clc
+	ret
+.failure:
+	pop ecx
+	pop edx
+	pop eax
+	stc
+	ret
+section .data
+	cyl  dw 0
+	head db 0
+	sec  db 0
