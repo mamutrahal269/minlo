dd89c3b6de1086aa694047b79bc845a83ea5d60f
mamutrahal269 <levrymvc@gmail.com>
2025-11-26 18:24:36 +0700

    lto

--- src/build.sh
+++ src/build.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 set -e
-i386-elf-gcc -nostdlib -nostdinc++ -nostdinc -ffreestanding -m32 -fno-pic -fno-pie -fno-rtti -fno-exceptions -fno-threadsafe-statics -masm=intel -c -I../include minlib.cpp
-i386-elf-gcc -nostdlib -nostdinc++ -nostdinc -ffreestanding -m32 -fno-pic -fno-pie -fno-rtti -fno-exceptions -fno-threadsafe-statics -masm=intel -c -I../include printf.cpp
-i386-elf-gcc -nostdlib -nostdinc++ -nostdinc -ffreestanding -m32 -fno-pic -fno-pie -fno-rtti -fno-exceptions -fno-threadsafe-statics -masm=intel -c -I../include internal/arith64.c
-i386-elf-gcc -nostdlib -nostdinc++ -nostdinc -ffreestanding -m32 -fno-pic -fno-pie -fno-rtti -fno-exceptions -fno-threadsafe-statics -masm=intel -c -I../include test.cpp
-i386-elf-gcc -nostdlib -nostdinc++ -nostdinc -ffreestanding -m32 -fno-pic -fno-pie -fno-rtti -fno-exceptions -fno-threadsafe-statics -masm=intel -c -I../include vbe.cpp
+
+CC=i386-elf-gcc
+CC_FLAGS="-Ofast -flto -ffunction-sections -fdata-sections -nostdlib -nostdinc++ -nostdinc -ffreestanding -m32 \
+-fno-pic -fno-pie -fno-rtti -fno-exceptions -fno-threadsafe-statics -masm=intel -I../include"
+
+$CC $CC_FLAGS -c minlib.cpp
+$CC $CC_FLAGS -c printf.cpp
+$CC $CC_FLAGS -c internal/arith64.c
+$CC $CC_FLAGS -c test.cpp
+$CC $CC_FLAGS -c vbe.cpp
+
 nasm -f elf int386.asm -o int386.o
 nasm -f elf -DDEBUG internal/initcom.asm -o initcom.o
 nasm -f bin -DLOAD_SECTORS=120 0.asm -o 0.bin
-ld -m elf_i386 -T ../link.ld -o boot.bin arith64.o int386.o minlib.o printf.o test.o vbe.o
+
+$CC -m32 -nostdlib -nostdinc -ffreestanding -flto -Wl,--gc-sections \
+-o boot.bin arith64.o int386.o minlib.o printf.o test.o vbe.o -T ../link.ld
+
 cat 0.bin boot.bin > minlo.bin
 dd if=minlo.bin of=../debug/c.img seek=0 bs=1 conv=notrunc
+
 cd ../debug
 bochs -q
-
--- src/internal/arith64.c
+++ src/internal/arith64.c
@@ -266,3 +266,62 @@ arith64_u64 __umoddi3(arith64_u64 a, arith64_u64 b)
     __divmoddi4(a, b, &r);
     return r;
 }
+arith64_u64 __udivmoddi4 (arith64_u64 n, arith64_u64 d, arith64_u64 *rp) {
+  arith64_u64 q = 0, r = n, y = d;
+  arith64_u32 lz1, lz2, i, k;
+
+  /* Implements align divisor shift dividend method. This algorithm
+     aligns the divisor under the dividend and then perform number of
+     test-subtract iterations which shift the dividend left. Number of
+     iterations is k + 1 where k is the number of bit positions the
+     divisor must be shifted left to align it under the dividend.
+     quotient bits can be saved in the rightmost positions of the dividend
+     as it shifts left on each test-subtract iteration. */
+
+  if (y <= r)
+    {
+      lz1 = __builtin_clzll (d);
+      lz2 = __builtin_clzll (n);
+
+      k = lz1 - lz2;
+      y = (y << k);
+
+      /* Dividend can exceed 2 ^ (width - 1) - 1 but still be less than the
+	 aligned divisor. Normal iteration can drops the high order bit
+	 of the dividend. Therefore, first test-subtract iteration is a
+	 special case, saving its quotient bit in a separate location and
+	 not shifting the dividend. */
+      if (r >= y)
+	{
+	  r = r - y;
+	  q =  (1ULL << k);
+	}
+
+      if (k > 0)
+	{
+	  y = y >> 1;
+
+	  /* k additional iterations where k regular test subtract shift
+	    dividend iterations are done.  */
+	  i = k;
+	  do
+	    {
+	      if (r >= y)
+		r = ((r - y) << 1) + 1;
+	      else
+		r =  (r << 1);
+	      i = i - 1;
+	    } while (i != 0);
+
+	  /* First quotient bit is combined with the quotient bits resulting
+	     from the k regular iterations.  */
+	  q = q + r;
+	  r = r >> k;
+	  q = q - (r << k);
+	}
+    }
+
+  if (rp)
+    *rp = r;
+  return q;
+}
