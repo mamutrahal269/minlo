e2b4f30b71a6ac1e7d1e2f9e411a0b36b78c20b5
mamutrahal269 <levrymvc@gmail.com>
2025-11-28 11:09:09 +0700

    -

--- debug/test.cpp
+++ debug/test.cpp
@@ -1,5 +1,7 @@
 #include <minlib.hpp>
+#include <vbe.hpp>
 [[gnu::section(".main"), noreturn, gnu::used]] void main() {
+	VBEmode_setup(mode_type::graphics, ~0, ~0, ~0);
     printf(outt::e9, "\033[31m ==========================================\r\nabubebbabbuebba\r\n==========================================\r\n");
     printf(outt::e9, "dsjhvkbvdfshjbklhijbvasdblhjivdaslhbjbvlhasdjklhjbkvadsbhjsdvablhjk");
     for(;;);
